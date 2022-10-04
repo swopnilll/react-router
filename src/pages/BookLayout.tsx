8e0048078b0df400f846f249ace93a80e8eb9eb7
Swopnil <leapfrog@pop-os.localdomain>
2022-10-04 08:59:21 +0545

    lesson-6: Nestet routes

---
+++ src/pages/BookLayout.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'react-router-dom'
+
+export const BookLayout = () => {
+    return (
+        <>
+            <Link to="/books/1" >Book 1</Link>
+            <br></br>
+            <Link to="/books/2" >Book 2</Link>
+            <br></br>
+            <Link to="/books/3" >Book 3</Link>
+            <br></br>
+            <Link to="/books/4" >Book 4</Link>
+            <br></br>
+            <Link to="/books/5" >Book 5</Link>
+            <br></br>
+            <Link to="/books/new">New Book</Link>
+            <br></br>
+            <Outlet />
+        </>
+    )
+}
